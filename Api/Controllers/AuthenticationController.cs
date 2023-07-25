36ac3e7d41c69c9aeedd41432c22268a81f29da1
ducknguyen <20520453@gm.uit.edu.vn>
2023-07-25 23:07:41 +0700

    Add project files.

---
+++ Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+
+using Contracts.Authentication;
+using Application.Server.Authentication;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("auth")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthenticationController(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+        var authResult = _authenticationService.Register(request.FirstName, request.LastName,
+            request.Email, request.Password);
+
+        var response = new AuthenticationResponse(
+            Guid.NewGuid(),
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token);
+        return Ok(response);
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+        var authResult = _authenticationService.Login(
+            request.Email, request.Password);
+
+        var response = new AuthenticationResponse(
+            Guid.NewGuid(),
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token);
+        return Ok(response);
+    }
+}
