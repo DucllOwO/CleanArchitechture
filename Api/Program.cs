90e0717049876ba937b55548a519d94114a18262
ducknguyen <20520453@gm.uit.edu.vn>
2023-07-26 09:44:28 +0700

    Part 1: project set up

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,18 +1,21 @@
+using Application;
+using Application.Server.Authentication;
+using Infrastucture;
+
 var builder = WebApplication.CreateBuilder(args);
 {
-  builder.Services.AddControllers();
-
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure();
+    builder.Services.AddControllers();
 }
 
 // Add services to the container.
 
-
-
 var app = builder.Build();
 {
-  app.UseHttpsRedirection();
-  app.MapControllers();
+    app.UseHttpsRedirection();
+    app.MapControllers();
 
-  app.Run();
+    app.Run();
 }
-
